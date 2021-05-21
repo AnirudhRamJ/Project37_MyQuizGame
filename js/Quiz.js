aac749d2f68c0fc9d20ea9373ea80f75f1d485d1
AnirudhRamJ <74857186+AnirudhRamJ@users.noreply.github.com>
2021-05-21 20:40:19 +0530

    Add files via upload

---
+++ js/Quiz.js
@@ -0,0 +1,59 @@
+class Quiz {
+  constructor() { }
+
+  getState() {
+    var gameStateRef = database.ref('gameState');
+    gameStateRef.on("value", function (data) {
+      gameState = data.val();
+    });
+  }
+
+  update(state) {
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  async start() {
+    if (gameState === 0) {
+      contestant = new Contestant();
+      var contestantCountRef = await database.ref('contestantCount').once("value");
+      if (contestantCountRef.exists()) {
+        contestantCount = contestantCountRef.val();
+        contestant.getCount();
+      }
+      question = new Question();
+      question.display();
+    }
+  }
+
+  play() {
+    question.hide();
+
+    background("lightBlue");
+    fill(0);
+
+    textSize(25);
+    text("Result of the quiz", 350, 50);
+
+    Contestant.getPlayerInfo();
+    if (allContestants !== undefined) {
+      fill("blue");
+      textSize(15);
+      text("NOTE: Contestant who answered correct is highlighted in green color!", 130, 230);
+
+      var displayPosition = 230;
+      for (var plr in allContestants) {
+        var correctAnswer = "2";
+        if (correctAnswer === allContestants[plr].answer) {
+          fill("green");
+        } else {
+          fill("red");
+        }
+        displayPosition = displayPosition + 20;
+        textSize(20);
+        text(allContestants[plr].name + ": " + allContestants[plr].answer, 200, displayPosition);
+      }
+    }
+  }
+}
